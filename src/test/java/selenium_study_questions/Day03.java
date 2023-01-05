4854fef4f9c7b3279369bca624042776260f7862
SerenUst <serenustfdm@gmail.com>
2023-01-05 23:53:00 +0300

    cookies

---
+++ src/test/java/selenium_study_questions/Day03.java
@@ -0,0 +1,58 @@
+package selenium_study_questions;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+
+public class Day03 extends TestBase {
+
+    @Test
+    public void test01() throws InterruptedException {
+        //1. http://zero.webappsecurity.com sayfasina gidin
+        driver.get("http://zero.webappsecurity.com");
+        Thread.sleep(1500);
+
+
+        //2. Signin buttonuna tiklayin
+        driver.findElement(By.cssSelector("[id='signin_button']")).click();
+        Thread.sleep(1500);
+
+        //3. Login alanine  "username" yazdirin
+        driver.findElement(By.cssSelector("[id='user_login']")).sendKeys("username");
+        Thread.sleep(1500);
+
+        //4. Password alanine "password" yazdirin
+        driver.findElement(By.cssSelector("[id='user_password']")).sendKeys("password");
+        Thread.sleep(1500);
+
+        //5. Sign in buttonuna tiklayin (hata mesaji icin back tusuna tiklayin)
+        driver.findElement(By.cssSelector("[class='btn btn-primary']")).click();
+        Thread.sleep(1500);
+        driver.navigate().back();
+        Thread.sleep(1500);
+        driver.navigate().back();
+        Thread.sleep(1500);
+
+        //6. Online Banking menusunden Pay Bills sayfasina gidin
+        driver.findElement(By.cssSelector("[id='onlineBankingMenu']")).click();
+        driver.findElement(By.cssSelector("[id='pay_bills_link']")).click();
+
+        //7. amount kismina yatirmak istediginiz herhangi bir miktari yazin
+        driver.findElement(By.cssSelector("[id='sp_amount']")).sendKeys("2");
+
+        //8. tarih kismina "2020-09-10" yazdirin
+        driver.findElement(By.cssSelector("[id='sp_date']")).sendKeys("2023-1-10");
+
+        //9. Pay buttonuna tiklayin
+        driver.findElement(By.cssSelector("[id='pay_saved_payees']")).click();
+
+        //10. "The payment was successfully submitted." mesajinin ciktigini kontrol edin
+        String actualText = driver.findElement(By.cssSelector("[id='alert_content']")).getText();
+        String expectedText = "The payment was successfully submitted.";
+
+        Assert.assertEquals(actualText,expectedText);
+
+    }
+
+}
---
+++ src/test/java/selenium_study_questions/Day04.java
@@ -0,0 +1,60 @@
+package selenium_study_questions;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+
+import java.sql.Driver;
+
+public class Day04 extends TestBase {
+static String urun;
+
+@Test
+
+   public void test() throws InterruptedException {
+
+//1. "https://www.saucedemo.com" Adresine gidin
+   driver.get("https://www.saucedemo.com");
+
+//2. Username kutusuna "standard_user" yazdirin
+   Thread.sleep(1500);
+driver.findElement(By.xpath("//*[@id='user-name']")).sendKeys("standard_user");
+
+
+//3. Password kutusuna "secret_sauce" yazdirin
+   Thread.sleep(1500);
+driver.findElement(By.xpath("//*[@id='password']")).sendKeys("secret_sauce");
+
+
+//4. Login tusuna basin
+   driver.findElement(By.xpath("//*[@id='login-button']")).click();
+   System.out.println(urun );
+
+
+//5. Ilk urunun ismini kaydedin ve bu urunun sayfasina gidin
+   String urun =driver.findElement(By.xpath("//*[text()='Sauce Labs Backpack']")).getText();
+   driver.findElement(By.xpath("//*[text()='Sauce Labs Backpack']")).click();
+   System.out.println(urun );
+
+//6. Add to Cart butonuna basin
+driver.findElement(By.xpath("//*[@id='add-to-cart-sauce-labs-backpack']")).click();
+
+//7. Alisveris sepetine tiklayin
+driver.findElement(By.xpath("//*[@class='shopping_cart_badge']")).click();
+
+//8. Sectiginiz urunun basarili olarak sepete eklendigini control edin
+   Thread.sleep(1500);
+String urun1=driver.findElement(By.xpath("//*[@class='inventory_item_name']")).getText();
+
+
+   Assert.assertEquals(urun1,urun);
+
+
+//9. Sayfayi kapatin
+   Thread.sleep(1500);
+driver.close();
+
+
+}
+}
